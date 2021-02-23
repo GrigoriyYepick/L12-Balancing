7e08f3a90b75659a5d55d21e5bb4c4aa964fa6fe
Grigoriy Yepick <grigoriy.yepick@live.com>
2021-02-23 11:49:54 +0200

    Initial commit

---
+++ App/ServerApi/Controllers/ServerController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace ServerApi.Controllers
+{
+    [ApiController]
+    public sealed class ServerController : ControllerBase
+    {
+        private const string DefaultResponse = "something went wrong";
+        private readonly ILogger<ServerController> _logger;
+
+        public ServerController(ILogger<ServerController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("/")]
+        public string Get()
+        {
+            var outputPath = Path.GetDirectoryName(new Uri(Assembly.GetAssembly(typeof(ServerController)).CodeBase).AbsolutePath);
+            var filePath = Path.Combine(outputPath, "server.txt");
+
+            string response = DefaultResponse;
+
+            if (System.IO.File.Exists(filePath))
+            {
+                response = System.IO.File.ReadAllText(filePath);
+            }
+
+            return response;
+        }
+    }
+}
